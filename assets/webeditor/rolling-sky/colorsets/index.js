c83ca05f36634bc548bf89939dfd3cb8852e7d0b
Pa AlyTo <Xiye1221@outlook.com>
2025-04-19 19:40:46 +0800

    Delete assets/webeditor/rolling-sky directory

--- assets/webeditor/rolling-sky/colorsets/index.js
+++
@@ -1,9 +0,0 @@
-console.log('== Hey... Whats up? ==')
-var rgbCopy
-window.onclick = e => {
-    rgbCopy = e.target.style.backgroundColor;
-	if (rgbCopy != '') {
-		console.log(rgbCopy);
-		navigator.clipboard.writeText(rgbCopy)
-	}
-}
